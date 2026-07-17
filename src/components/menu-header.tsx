002f8be1d83870bad5df9c4446dd2aee27e8ace6
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-18 02:40:01 +0700

    feat(menu-header): add pathname and change text color while active

--- src/components/menu-header.tsx
+++ src/components/menu-header.tsx
@@ -1,12 +1,14 @@
+"use client";
 import Link from "next/link";
-import React from "react";
 import {ChildMotion, WrapperMotion} from "./ui/wrapper-motion";
 import {cn} from "@/libs/utils";
+import {usePathname} from "next/navigation";
+import {motion} from "motion/react";
 
 const menuData = [
   {name: "services", link: "/services"},
   {name: "industries", link: "/industries"},
-  {name: "about us", link: "/about us "},
+  {name: "about us", link: "/about"},
   {name: "team dapixel", link: "/team"},
   {name: "blog", link: "/blog"},
 ];
@@ -16,17 +18,33 @@ interface MenuHeaderProps {
 }
 
 const MenuHeader = ({className}: MenuHeaderProps) => {
+  const pathname = usePathname();
   return (
     <nav aria-label="menu header">
       <WrapperMotion>
         <ul className={cn("flex items-center gap-11", className)}>
-          {menuData.map((item) => (
-            <ChildMotion key={item.name}>
-              <li className="text-[18px] font-semibold text-black capitalize">
-                <Link href={item.link}>{item.name}</Link>
-              </li>
-            </ChildMotion>
-          ))}
+          {menuData.map((item) => {
+            const isActive = pathname === item.link;
+            return (
+              <ChildMotion key={item.name}>
+                <motion.li
+                  whileHover={{scale: 1.1}}
+                  whileTap={{scale: 0.9}}
+                  animate={{
+                    scale: isActive ? 1.1 : 1,
+                  }}
+                  transition={{type: "spring", damping: 15, stiffness: 150}}
+                  className={cn(
+                    "text-[18px] font-semibold capitalize",
+                    isActive ? "text-orange-300" : "text-black",
+                    "transition-colors duration-200 ease-in-out",
+                  )}
+                >
+                  <Link href={item.link}>{item.name}</Link>
+                </motion.li>
+              </ChildMotion>
+            );
+          })}
         </ul>
       </WrapperMotion>
     </nav>
