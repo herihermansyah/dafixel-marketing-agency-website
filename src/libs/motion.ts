8aa106c753fab1926c750122c6032ade4c234866
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-17 14:21:56 +0700

    feat(libs): add utils clsx, tailwind-merge, and motion animation wrappers

---
+++ src/libs/motion.ts
@@ -0,0 +1,30 @@
+import {Variants} from "motion/react";
+
+export const wrapperMotion: Variants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.1,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+export const childMotion: Variants = {
+  hidden: {
+    opacity: 0,
+    y: -50,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      type: "spring",
+      damping: 15,
+      stiffness: 100,
+    },
+  },
+};
