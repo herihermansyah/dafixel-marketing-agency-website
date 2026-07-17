24e3cdd7cd55f4320f69bdaf1c1f7a147e369a1a
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-17 23:18:58 +0700

    feat(wrapper-motion): add wrapper and child motion

---
+++ src/components/ui/wrapper-motion.tsx
@@ -0,0 +1,67 @@
+"use client";
+import React, {ComponentProps, forwardRef} from "react";
+import {motion} from "motion/react";
+import {childMotion, wrapperMotion} from "@/libs/motion";
+
+// type motion div props
+type MotionDivProps = ComponentProps<typeof motion.div>;
+
+interface WrapperMotionType extends MotionDivProps {
+  children?: React.ReactNode;
+}
+
+// ====================== wrapper    motion ======================
+
+const WrapperMotion = forwardRef<HTMLDivElement, WrapperMotionType>(
+  ({children, variants, initial, animate, viewport, ...props}, ref) => {
+    // check whether the user is using the "whileInView"
+    const hashWhileInView = props.whileInView !== undefined;
+
+    return (
+      <motion.div
+        //   deafult variants
+        variants={variants || wrapperMotion}
+        // the first open animation
+        initial={initial !== undefined ? initial : "hidden"}
+        // logic when user use animate or whileInView
+        animate={
+          animate != undefined
+            ? animate
+            : hashWhileInView
+              ? undefined
+              : "visible"
+        }
+        // logic when user use whileInView
+        viewport={
+          viewport !== undefined
+            ? viewport
+            : hashWhileInView
+              ? {once: true, amount: 0.2}
+              : undefined
+        }
+        {...props}
+        ref={ref}
+      >
+        {children}
+      </motion.div>
+    );
+  },
+);
+
+WrapperMotion.displayName = "WrapperMotion";
+
+// ====================== child motion ======================
+
+const ChildMotion = forwardRef<HTMLDivElement, WrapperMotionType>(
+  ({children, ...props}, ref) => {
+    return (
+      <motion.div variants={childMotion} ref={ref} {...props}>
+        {children}
+      </motion.div>
+    );
+  },
+);
+
+ChildMotion.displayName = "ChildMotion";
+
+export {WrapperMotion, ChildMotion};
