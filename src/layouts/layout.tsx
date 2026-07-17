261708f88fd203888f074094e5d5f91ae97d2c01
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-17 23:21:14 +0700

    feat(layout header): add header main layout

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Header from "./header";
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
     <div className="flex flex-col min-h-screen">
-      <header className="sticky top-0 z-50"></header>
-      <main className="grow">{children}</main>
+      <header className="sticky top-0 z-50">
+        <Header />
+      </header>
+      <main className="grow my-5">{children}</main>
       <footer></footer>
     </div>
   );
