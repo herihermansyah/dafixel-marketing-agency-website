6d2702bf33007ee2320dc36bbd9409f5e6903e34
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-18 22:50:46 +0700

    feat: add how it works section and style flex-col

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/sections/home/hero";
+import HowItWorks from "@/components/sections/home/how-it-works";
 import React from "react";
 
 const page = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-30">
       <Hero />
+      <HowItWorks />
     </div>
   );
 };
