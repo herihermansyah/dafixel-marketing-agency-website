458f4ebdd5ff718d144dc8e9afc7748f0f9ed73a
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-19 22:45:16 +0700

    style(blog card): size text

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {ChildMotion} from "./ui/wrapper-motion";
+import {ChildMotion, WrapperMotion} from "./ui/wrapper-motion";
 import Button from "./ui/button";
+import WrapperContent from "./ui/wrapper-content";
 
 const blogData = Array.from({length: 20}).map((_, i) => ({
   id: 1 + i,
@@ -11,24 +12,28 @@ const blogData = Array.from({length: 20}).map((_, i) => ({
 
 const BlogCard = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-      {blogData.map((item) => (
-        <ChildMotion
-          className="bg-white rounded-3xl flex flex-col gap-5 p-5 shadow-drop-down"
-          key={item.id}
-        >
-          <ChildMotion>
-            <h3 className="font-bold text-[44px] leading-13.75">
-              {item.title}
-            </h3>
-          </ChildMotion>
-          <ChildMotion>
-            <p className="text-[14px] leading-7">{item.descrtiption}</p>
-          </ChildMotion>
-          <Button>Read More</Button>
-        </ChildMotion>
-      ))}
-    </div>
+    <WrapperMotion whileInView={"visible"}>
+      <WrapperContent>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+          {blogData.map((item) => (
+            <ChildMotion
+              className="bg-white rounded-3xl flex flex-col gap-5 p-5 shadow-drop-down"
+              key={item.id}
+            >
+              <ChildMotion>
+                <h3 className="font-bold text-[28px] leading-13.75">
+                  {item.title}
+                </h3>
+              </ChildMotion>
+              <ChildMotion>
+                <p className="text-[18px] leading-7">{item.descrtiption}</p>
+              </ChildMotion>
+              <Button>Read More</Button>
+            </ChildMotion>
+          ))}
+        </div>
+      </WrapperContent>
+    </WrapperMotion>
   );
 };
 
