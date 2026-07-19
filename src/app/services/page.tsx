42bccc2b3ce9de168f8e72a4a082991fec982d59
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-20 02:31:29 +0700

    feat(service page): import components

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import ServiceHero from "@/components/sections/service-hero";
+import ServiceCard from "@/components/service-card";
+import ContentOverivew from "@/components/sections/content-overivew";
+import GetInTouch from "@/components/get-in-touch";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex flex-col gap-30">
+      <ServiceHero />
+      <ServiceCard />
+      <ContentOverivew />
+      <GetInTouch />
+    </div>
+  );
+};
 
-export default page
+export default page;
