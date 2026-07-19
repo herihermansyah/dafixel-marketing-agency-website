34c64a098872654bbd98cd0a43f62ddf1e064403
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-20 02:31:59 +0700

    feat(home page): fix component import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import HomeCompounded from "@/components/sections/home/home-compounded";
-import React from "react";
+import ConsultationCta from "@/components/consultation-cta";
+import RecentsBlog from "@/components/recents-blog";
+import Hero from "@/components/sections/hero";
+import HowItWorks from "@/components/how-it-works";
+import Testimonials from "@/components/testimonials";
 
 const page = () => {
   return (
-    <div>
-      <HomeCompounded />
+    <div className="flex flex-col gap-30">
+      <Hero />
+      <HowItWorks />
+      <ConsultationCta />
+      <Testimonials />
+      <RecentsBlog />
     </div>
   );
 };
