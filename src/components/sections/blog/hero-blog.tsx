86a89a9900be13e0bd402511cc2df67f57a37676
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-19 22:37:27 +0700

    feat(blog hero): add hero blog

---
+++ src/components/sections/blog/hero-blog.tsx
@@ -0,0 +1,36 @@
+import Shape001 from "@/assets/shape-001";
+import WrapperContent from "@/components/ui/wrapper-content";
+import {
+  ChildMotion,
+  FadeMotion,
+  WrapperMotion,
+} from "@/components/ui/wrapper-motion";
+import React from "react";
+
+const HeroBlog = () => {
+  return (
+    <WrapperMotion>
+      <section className="relative overflow-hidden h-115">
+        <WrapperContent>
+          <div className="flex flex-col items-start gap-3 relative z-10 ">
+            <ChildMotion>
+              <h2 className="text-[53px] md:text-[76px] font-bold leading-[130%] font-plus-jakarta-sans">
+                DaPixel Blog
+              </h2>
+            </ChildMotion>
+            <ChildMotion>
+              <p className="text-[24px] text-orange-300 font-medium leading-[110%]">
+                Digital marketing articles
+              </p>
+            </ChildMotion>
+          </div>
+          <FadeMotion>
+            <Shape001 className="absolute bottom-0 left-1/2 -translate-x-1/2" />
+          </FadeMotion>
+        </WrapperContent>
+      </section>
+    </WrapperMotion>
+  );
+};
+
+export default HeroBlog;
