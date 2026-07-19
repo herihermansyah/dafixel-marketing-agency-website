b61980a4c0e82b3f9fbdb2033f5f59f31eb759b1
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-19 18:40:58 +0700

    feat(home compounded): fix wrapper content consultation

--- src/components/sections/home/home-compounded.tsx
+++ src/components/sections/home/home-compounded.tsx
@@ -1,23 +1,19 @@
 import ConsultationCta from "@/components/consultation-cta";
+import RecentsBlog from "@/components/recents-blog";
 import Hero from "@/components/sections/home/hero";
 import HowItWorks from "@/components/sections/home/how-it-works";
-import WrapperContent from "@/components/ui/wrapper-content";
-import {WrapperMotion} from "@/components/ui/wrapper-motion";
-import React from "react";
+import Testimonials from "@/components/testimonials";
+import WhyDapixel from "@/components/why-dapixel";
 
 const HomeCompounded = () => {
   return (
     <div className="flex flex-col gap-30">
       <Hero />
       <HowItWorks />
-      <WrapperMotion
-        whileInView={"visible"}
-        className="bg-orange-300 py-30 lg:py-62"
-      >
-        <WrapperContent className="px-20 xl:px-20">
-          <ConsultationCta />
-        </WrapperContent>
-      </WrapperMotion>
+      <ConsultationCta />
+      <Testimonials />
+      <RecentsBlog />
+      <WhyDapixel />
     </div>
   );
 };
