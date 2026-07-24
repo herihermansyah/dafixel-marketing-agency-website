9ef91ae46ffb10832ef07402b57ba2907624a55c
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-24 13:43:13 +0700

    feat(industries page): add industries page

--- src/app/industries/page.tsx
+++ src/app/industries/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import ExpertiseCard from "@/components/expertise-card";
+import GetInTouch from "@/components/get-in-touch";
+import EditorialGuideDownload from "@/components/sections/editorial-guide-download";
+import IndustriesHero from "@/components/sections/industries-hero";
+import IndustriesOverview from "@/components/sections/industries-overview";
+import Testimonials from "@/components/testimonials";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="flex flex-col gap-30">
+      <IndustriesHero />
+      <ExpertiseCard />
+      <IndustriesOverview />
+      <Testimonials />
+      <EditorialGuideDownload />
+      <GetInTouch />
+    </div>
+  );
+};
 
-export default page
+export default page;
