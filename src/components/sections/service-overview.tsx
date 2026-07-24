0baa9caa875475593e3c659af8a358e36eb7bb2d
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-24 13:40:32 +0700

    feat(service overview): create service overview

---
+++ src/components/sections/service-overview.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import {DescriptionOverview, Overview, TitleOverview} from "../ui/overview";
+
+const ServiceOverview = () => {
+  return (
+    <Overview>
+      <TitleOverview>What happens when i hire lunar strategry?</TitleOverview>
+      <DescriptionOverview>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+        consequuntur accusamus nesciunt beatae officia asperiores perferendis
+        odio expedita accusantium? Cumque, aliquam repellat autem qui eaque
+        impedit hic iure rerum ratione minus consequuntur facilis nobis
+        molestiae temporibus tempore? Perspiciatis, neque quia?
+      </DescriptionOverview>
+    </Overview>
+  );
+};
+
+export default ServiceOverview;
