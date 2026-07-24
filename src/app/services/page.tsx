5d85250be74eec0091f9d7f7d32aff649e44a32f
Heri Hermansyah <heryhermansyah563@gmail.com>
2026-07-24 13:43:41 +0700

    feat(service page): add service page

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ServiceHero from "@/components/sections/service-hero";
 import ServiceCard from "@/components/service-card";
-import ContentOverivew from "@/components/sections/content-overivew";
 import GetInTouch from "@/components/get-in-touch";
+import ServiceOverview from "@/components/sections/service-overview";
 
 const page = () => {
   return (
     <div className="flex flex-col gap-30">
       <ServiceHero />
       <ServiceCard />
-      <ContentOverivew />
+      <ServiceOverview />
       <GetInTouch />
     </div>
   );
